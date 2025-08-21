2928e1a163aaee6f4e039b2f83a3957a17af0aaf
Rolex-444 <sivaraman.gc49@gmail.com>
2025-08-21 15:32:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from bot import Bot
 import pyrogram.utils
+import webserver             # <--- Add this line
 
 pyrogram.utils.MIN_CHANNEL_ID = -1002297453351
+
+webserver.start()            # <--- Add this line to start the web server
+
 Bot().run()
